cd35ca1bddda035fb83180107e6d32e3f5dbca66
sangshu329 <sangshu.sun@163.com>
2017-05-28 16:59:19 +0800

    2017年5月28日16:59:17

--- functions.php
+++ functions.php
@@ -1,10 +1,11 @@
 <?php
 /* 注意目录路径问题 */
-function mylog($key, $value)
+function mylog($key, $value, $filename = 'app_log')
 {
-    if(is_array($value) || is_object($value)) {
+    $path = str_replace('\\', '/', str_replace('system/common.php', '', __FILE__));
+    if (is_array($value) || is_object($value)) {
         $value = serialize($value);
     }
-    if(!is_dir('logs')) @mkdir('logs',0777,true);
-    file_put_contents('logs/thiswaplog.log', '[' .     date('Y-m-d H:i:s') . ']  ' . $key . '->' . $value . "\r\n", FILE_APPEND | LOCK_EX);
+    if (!is_dir('logs')) @mkdir('logs', 0777, true);
+    file_put_contents($path . 'logs/' . $filename . date('Ymd') . '.log', '[' . date('Y-m-d H:i:s') . ']  ' . $key . '->' . $value . "\r\n", FILE_APPEND | LOCK_EX);
 }
